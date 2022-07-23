a7c1d610c0e078fbf28d77547fc884fddaaed6fc
Sergey_Tyutvin <tyutvi@gmail.com>
2022-07-23 11:39:26 +0300

    Создание метода для заполнения массива

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк,
+//длина которых меньше либо равна 3 символа.
+string[] existingArray= new string[] {"London", "Tokyo", "Moscow", "Washington", "Berlin", "Madrid", "Paris"};
+void FillArray(string[] rndArray) //задание метода для заполнения какого-то массива с заданным количеством случайных элементов
+{
+    for (int index = 0; index < rndArray.Length; index++)
+    {
+        rndArray[index] = existingArray [new Random().Next(0, existingArray.Length)];
+    }
+}
