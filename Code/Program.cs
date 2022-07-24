5a8a13fce1e6da9d2d6919199373cd752c6e46be
STyutvin <106702728+STyutvin@users.noreply.github.com>
2022-07-24 12:55:38 +0300

    Корректировка кода.
    Корректировка кода в соответствии с условием задачи.

--- Code/Program.cs
+++ Code/Program.cs
@@ -4,14 +4,8 @@
     {
         //Написать программу, которая из имеющегося массива строк формирует массив из строк,
         //длина которых меньше либо равна 3 символа.
-        string[] existingArray = new string[] { "London", "Tokyo", "Moscow", "Washington", "Berlin", "Madrid", "Paris" };
-        void FillArray(string[] rndArray) //задание метода для заполнения какого-то массива с заданным количеством случайных элементов
-        {
-            for (int index = 0; index < rndArray.Length; index++)
-            {
-                rndArray[index] = existingArray[new Random().Next(0, existingArray.Length)];
-            }
-        }
+        string[] existingArray = new string[] { "hello", "2", "Moscow", "one", "))", "madness", "off" };
+
         void PrintArray(string[] prntArray) //задание метода для вывода элементов любого массива на консоль
         {
             for (int position = 0; position < prntArray.Length; position++)
@@ -20,13 +14,13 @@
             }
             Console.WriteLine();
         }
-        string[] newArray = new string[new Random().Next(0, 4)];
-
-        FillArray(newArray);
         Console.WriteLine("Существующий строковый массив:");
         PrintArray(existingArray);
         Console.WriteLine();
         Console.WriteLine("Новый строковый массив:");
-        PrintArray(newArray);
+        for (int i = 0; i < existingArray.Length; i++)
+            {
+                if (existingArray[i].Length<=3) Console.Write(existingArray[i] + " ");
+            }
     }
 }
